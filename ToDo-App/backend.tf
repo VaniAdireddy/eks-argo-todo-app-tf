ea4509a59a7842456034c1ad403b80cadadc3cd3
VaniAdireddy <vasanthaadireddy@gmail.com>
2024-10-01 18:04:23 -0400

    s3 bucket name updated

--- ToDo-App/backend.tf
+++ ToDo-App/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eks-argo-todo-app-tf-bucket"
+    bucket = "eks-argo-todo-app-tf-bucket-vasantha"
     key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "eks-argo-todo-app-tf-dynamo"
